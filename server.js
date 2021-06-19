bc2141d0a085967ed459906b2c90ca3bda51f82f
Josh Nosal <joshnosal@joshs-imac.lan>
2021-06-19 18:39:18 -0500

    something

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+// Test Script
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
